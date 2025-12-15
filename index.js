b5d26c6ba1d3c9cca44f5ae61e94492702359269
Matthew <killerfoxgaming08712@gmail.com>
2025-12-15 14:51:52 -0500

    website now mirrors the idea

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-function addCounter(item) {
-    const counters = document.getElementsByClassName("counter");
-    console.log(counters)
+function textBlink() {
+    const text = document.getElementById("info");
+    while (true) {
+        while (Number(text.style.opacity.split("%"))[0] != 0) {
+            text.style.opacity = `${Number(text.style.opacity) - 5}`;
+        }
+        while (Number(text.style.opacity.split("%"))[0] != 0) {
+            text.style.opacity = `${Number(text.style.opacity) + 5}`;
+        }
+    }
+}
+
+function textContinue() {
+    const sentences = document.getElementsByClassName("hidden")
 }
